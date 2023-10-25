7be10a3e6eeee8a5bb45726487bc64fc9838eb52
DolonStoned <148004072+DolonStoned@users.noreply.github.com>
2023-10-25 17:46:26 +0100

    Add files via upload

--- PlayerProjectilescripts/BulletMovement.cs
+++ PlayerProjectilescripts/BulletMovement.cs
@@ -8,9 +8,33 @@ public class BulletMovement : MonoBehaviour
     public Rigidbody2D rb;
     public Vector2 movement;
     
+
+    private void Start()
+    {
+
+        GameObject slime = GameObject.FindGameObjectWithTag("MySlime");
+        Physics2D.IgnoreCollision(slime.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+        
+    }
     void Update()
     {
+        Physics2D.IgnoreLayerCollision(3, 3);
+        //GameObject[] projectiles = GameObject.FindGameObjectsWithTag("Projectile");
+        //print(projectiles[0]);
+        //if (projectiles[0] != null)
+        //
+        //{
+        //    
+        //    for (int i = 0; i < projectiles.Length; i++)
+        //    {
+        //        Physics2D.IgnoreCollision(projectiles[i].GetComponent<Collider2D>(), GetComponent<Collider2D>());
+        //    }
+        //}
         rb.MovePosition(rb.position + movement * bullet_speed * Time.fixedDeltaTime);
+        
     }
-
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
 }
